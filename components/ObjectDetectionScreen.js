d16d985f519d97579b123490d83dc4b4419d9dd9
Eizen724 <eliott.boichot@gmail.com>
2023-11-15 16:11:54 +0100

    commit add camera in the botom right of the screen

--- components/ObjectDetectionScreen.js
+++ components/ObjectDetectionScreen.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
-import { Camera } from 'expo-camera';
-import axios from 'axios';
-import { useSearchParams } from 'react-router-native';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import axios from "axios";
+import { useSearchParams } from "react-router-native";
 
 const ObjectDetectionScreen = () => {
   const [cameraRef, setCameraRef] = useState(null);
@@ -11,54 +11,47 @@ const ObjectDetectionScreen = () => {
   const [isCameraReady, setIsCameraReady] = useState(false);
   const [searchParams] = useSearchParams();
   const ApiUrl = searchParams.get("url");
-  const detectionInterval = 5000; 
-  const [cpt, setCpt] = useState(0);
+  const detectionInterval = 5000;
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
   const takePicture = async () => {
-    setCpt(cpt + 1); 
     if (isCameraReady && cameraRef && hasPermission) {
       try {
         const options = { quality: 0.5, base64: true };
         const { uri } = await cameraRef.takePictureAsync(options);
 
         const formData = new FormData();
-        formData.append('image', {
+        formData.append("image", {
           uri,
-          name: 'image.jpg',
-          type: 'image/jpeg',
+          name: "image.jpg",
+          type: "image/jpeg",
         });
 
         try {
-          const response = await axios.post(
-            `${ApiUrl}/detect`,
-            formData,
-            {
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              },
-            }
-          );
-
+          const response = await axios.post(`${ApiUrl}/detect`, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          });
           const detectedObjects = response.data.image_with_objects;
           setDetectedObjects(detectedObjects);
         } catch (error) {
-          console.error('Error sending frame for detection:', error);
+          console.error("Error sending frame for detection:", error);
         }
       } catch (error) {
-        console.error('Error taking picture:', error);
+        console.error("Error taking picture:", error);
       }
     }
   };
 
   const handleCameraReady = () => {
     setIsCameraReady(true);
-    takePicture(); 
+    takePicture();
   };
 
   useEffect(() => {
@@ -71,26 +64,39 @@ const ObjectDetectionScreen = () => {
     };
   }, [isCameraReady, hasPermission]);
 
+  const saveToGallery = async () => {
+  
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.detectedObjects}>
+        <Image
+          source={{ uri: `data:image/jpeg;base64, ${detectedObjects}` }}
+          style={{
+            height: Dimensions.get("window").height,
+            width: Dimensions.get("window").width,
+          }}
+        />
+      </View>
+
       {hasPermission === null ? (
         <Text>Requesting camera permission...</Text>
       ) : hasPermission === false ? (
         <Text>No access to the camera</Text>
       ) : (
         <View style={styles.cameraContainer}>
-          <Text style={styles.cptText}>{cpt}</Text>
           <Camera
             style={styles.camera}
             type={Camera.Constants.Type.back}
             ref={(ref) => setCameraRef(ref)}
             onCameraReady={handleCameraReady}
           />
+          <TouchableOpacity style={styles.captureButton} onPress={saveToGallery}>
+            <Text style={styles.captureButtonText}>Capture & Save</Text>
+          </TouchableOpacity>
         </View>
       )}
-      <View style={styles.detectedObjects}>
-        <Image source={{ uri: `data:image/jpeg;base64, ${detectedObjects}` }} style={{ height: Dimensions.get('window').height, width: Dimensions.get('window').width, }} />
-      </View>
     </View>
   );
 };
@@ -98,30 +104,51 @@ const ObjectDetectionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   cameraContainer: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-    overflow: 'hidden',
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    width: "50%",
+    height: "50%",
+    overflow: "hidden",
+    borderWidth: 2,
+    borderColor: "slateblue",
+    borderRadius: 6,
+    zIndex: 1
   },
   camera: {
     flex: 1,
   },
   detectedObjects: {
-    position: 'absolute',
+    // position: "relative",
+    // zIndex: 0
   },
   objectLabel: {
     fontSize: 18,
     marginBottom: 5,
   },
   cptText: {
-    position: 'absolute',
+    position: "absolute",
     top: 20,
     right: 20,
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    color: "#fff", 
+  },
+  captureButton: {
+    position: "absolute",
+    bottom: 20,
+    left: 20, 
+    backgroundColor: "#3498db", 
+    padding: 15,
+    borderRadius: 10,
+  },
+  captureButtonText: {
+    color: "#fff", 
+    fontSize: 16,
   },
 });
 
--- components/QRCodeScanner.js
+++ components/QRCodeScanner.js
@@ -1,29 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, TextInput, Linking, StyleSheet } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Linking,
+  StyleSheet,
+} from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 import { useNavigate } from "react-router-native";
+import axios from "axios";
 
 const QRCodeScanner = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [manualUrl, setManualUrl] = useState('');
+  const [manualUrl, setManualUrl] = useState("");
+  const [inputError, setInputError] = useState(false);
+  const [qrCodeBorderColor, setQRCodeBorderColor] = useState("white");
   const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async ({ type, data }) => {
+    if (scanned) return;
+
     setScanned(true);
-    navigate(`ObjectDetection?url=${data}`);
+
+    try {
+      const result = await verificationURL(data);
+
+      if (result === true) {
+        navigate(`ObjectDetection?url=${data}`);
+      } else {
+        setInputError(true);
+        setQRCodeBorderColor("red");
+        setTimeout(() => {
+          setQRCodeBorderColor("white");
+          setScanned(false); // Allow scanning again after a delay
+        }, 2000); // Adjust the delay as needed (in milliseconds)
+      }
+    } catch (error) {
+      console.error("Error during QR code verification:", error);
+      setInputError(true);
+      setQRCodeBorderColor("red");
+      setTimeout(() => {
+        setQRCodeBorderColor("white");
+        setScanned(false); // Allow scanning again after a delay
+      }, 2000); // Adjust the delay as needed (in milliseconds)
+    }
   };
 
-  const handleUrlSubmit = () => {
-    if (manualUrl.trim() !== '') {
-      navigate(`ObjectDetection?url=${manualUrl}`);
+  const handleUrlSubmit = async () => {
+    try {
+      if (manualUrl.trim() !== "") {
+        const result = await verificationURL(manualUrl);
+        if (result === true) {
+          navigate(`ObjectDetection?url=${manualUrl}`);
+        } else {
+          setInputError(true);
+        }
+      } else {
+        setInputError(true);
+      }
+    } catch (error) {
+      console.error("Error during URL verification:", error);
+      setInputError(true);
+    }
+  };
+
+  const verificationURL = async (url) => {
+    try {
+      const response = await axios.get(`${url}/verification`);
+      return response.data === "True";
+    } catch (error) {
+      return false;
     }
   };
 
@@ -35,28 +90,35 @@ const QRCodeScanner = () => {
   }
 
   const handleExternalLink = () => {
-    Linking.openURL('https://colab.research.google.com/github/Eizen724/PrProj/blob/main/PR_PROJ.ipynb');
+    Linking.openURL(
+      "https://colab.research.google.com/github/Eizen724/PrProj/blob/main/PR_PROJ.ipynb"
+    );
   };
 
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.input}
+        style={[styles.input, inputError && styles.inputError]}
         placeholder="Enter URL manually"
         placeholderTextColor="gray"
-        onChangeText={(text) => setManualUrl(text)}
+        onChangeText={(text) => {
+          setManualUrl(text);
+          setInputError(false);
+        }}
         onSubmitEditing={handleUrlSubmit}
         value={manualUrl}
       />
       <View style={styles.cameraContainer}>
-        <View style={styles.square}>
+        <View style={[styles.square, { borderColor: qrCodeBorderColor }]}>
           <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            onBarCodeScanned={handleBarCodeScanned}
             style={{ flex: 1 }}
           />
         </View>
       </View>
-      <Text style={styles.text}>Scan Your object detection QR code. You can get a QR code by clicking&nbsp;
+      <Text style={styles.text}>
+        Scan Your object detection QR code. You can get a QR code by
+        clicking&nbsp;
         <TouchableOpacity onPress={handleExternalLink}>
           <Text style={styles.linkText}>here</Text>
         </TouchableOpacity>
@@ -68,42 +130,45 @@ const QRCodeScanner = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
   },
   text: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
     marginBottom: 20,
   },
   linkText: {
-    color: 'blue',
-    textDecorationLine: 'underline',
+    color: "blue",
+    textDecorationLine: "underline",
     fontSize: 24,
   },
   cameraContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
     aspectRatio: 1,
   },
   square: {
-    width: '80%',
+    width: "80%",
     aspectRatio: 1,
     borderWidth: 2,
-    borderColor: 'white',
-    overflow: 'hidden',
+    borderColor: "white",
+    overflow: "hidden",
   },
   input: {
     marginTop: "20%",
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
-    color: 'white',
+    color: "white",
     margin: 20,
     paddingHorizontal: 10,
+  },
+  inputError: {
+    borderColor: "red",
   },
 });
 
